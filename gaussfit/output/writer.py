52a21e80ed1812fb631d982c90f86d67654f0931
Ryan Chiechi <r.c.chiechi@rug.nl>
2021-03-25 17:32:11 +0100

    Let's find the underlying bug

--- gaussfit/output/writer.py
+++ gaussfit/output/writer.py
@@ -300,16 +300,13 @@ class Writer():
     def WriteFN(self):
         '''Write Fowler-Nordheim plots of input data.'''
         _fn = os.path.join(self.opts.out_dir,self.opts.outfile+"_FN.txt")
-        try:
-            with open(_fn, 'w', newline='') as csvfile:
-                writer = csv.writer(csvfile, dialect='JV')
-                writer.writerow(['1/V'] + ['Y_%d'%x for x in range(1,len( self.XY[list(self.XY.keys())[0]]['FN'] )+1)])
-                for x in self.XY:
-                    if x == 0.0:
-                        continue
-                    writer.writerow(['%0.4f' % (1/x)] + list(self.XY[x]['FN']))
-        except IndexError:
-            logger.error("Error writing FN data.")
+        with open(_fn, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, dialect='JV')
+            writer.writerow(['1/V'] + ['Y_%d'%x for x in range(1,len( self.XY[list(self.XY.keys())[0]]['FN'] )+1)])
+            for x in self.XY:
+                if x == 0.0:
+                    continue
+                writer.writerow(['%0.4f' % (1/x)] + list(self.XY[x]['FN']))
 
     def WriteFilteredGauss(self):
         '''Write the Gaussian-derived J/V data derived from the filtered input data
