9def7926f960aa7b622fd670c6cb4618d99c0f61
Ryan Chiechi <r.c.chiechi@rug.nl>
2021-04-20 15:58:30 +0200

    CI in R, NDC and DJDV

--- gaussfit/output/writer.py
+++ gaussfit/output/writer.py
@@ -394,29 +394,50 @@ class Writer():
         with open(_fn, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, dialect='JV')
             if self.opts.logr:
-                writer.writerow(["Potential (V)","log |R|","Standard Deviation"])
+                writer.writerow(["Potential (V)",
+                                "log |R|",
+                                "Standard Deviation",
+                                "%s%% confidence interval" % (100*(1 - self.opts.alpha))])
             else:
-                writer.writerow(["Potential (V)","|R|","Standard Deviation"])
-            #Y = []
-            #Yerr = []
+                writer.writerow(["Potential (V)",
+                                "|R|",
+                                "Standard Deviation",
+                                "%s%% confidence interval" % (100*(1 - self.opts.alpha))])
             for x in self.XY:
-                writer.writerow(['%f'%x,'%f'%self.XY[x]['R']['hist']['mean'],'%f'%self.XY[x]['R']['hist']['std']])
+                _sem = float(self.XY[x]['R']['hist']['std'])/np.sqrt(len(self.opts.in_files)-1 or 1)
+                _t_val = _sem * stdtrit( len(self.opts.in_files)-1 or 1, 1 - self.opts.alpha )
+                writer.writerow(['%0.4f'%x,
+                                 '%0.4f'%self.XY[x]['R']['hist']['mean'],
+                                 '%0.4f'%self.XY[x]['R']['hist']['std'],
+                                 "%0.4f"% _t_val])
         _fn = os.path.join(self.opts.out_dir,self.opts.outfile+"_logdJdVGauss.txt")
         with open(_fn, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, dialect='JV')
-            writer.writerow(["Potential (V)","Log|dJ/dV|","Standard Deviation"])
-            #Y = []
-            #Yerr = []
+            writer.writerow(["Potential (V)",
+                             "Log|dJ/dV|",
+                            "Standard Deviation"],
+                            "%s%% confidence interval" % (100*(1 - self.opts.alpha)))
             for x in self.GHists:
-                writer.writerow(['%f'%x,'%f'%self.GHists[x]['hist']['mean'],'%f'%self.GHists[x]['hist']['std']])
+                _sem = float(self.GHists[x]['hist']['std'])/np.sqrt(len(self.opts.in_files)-1 or 1)
+                _t_val = _sem * stdtrit( len(self.opts.in_files)-1 or 1, 1 - self.opts.alpha )
+                writer.writerow(['%0.4f'%x,
+                                 '%0.4f'%self.GHists[x]['hist']['mean'],
+                                 '%0.4f'%self.GHists[x]['hist']['std'],
+                                 "%0.4f"% _t_val])
         _fn = os.path.join(self.opts.out_dir,self.opts.outfile+"_NDCGauss.txt")
         with open(_fn, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, dialect='JV')
-            writer.writerow(["Potential (V)","dJ/dV * V/J","Standard Deviation"])
-            #Y = []
-            #Yerr = []
+            writer.writerow(["Potential (V)",
+                             "dJ/dV * V/J",
+                             "Standard Deviation",
+                             "%s%% confidence interval" % (100*(1 - self.opts.alpha))])
             for x in self.GHists:
-                writer.writerow(['%f'%x,'%f'%self.NDCHists[x]['hist']['mean'],'%f'%self.NDCHists[x]['hist']['std']])
+                _sem = float(self.NDCHists[x]['hist']['std'])/np.sqrt(len(self.opts.in_files)-1 or 1)
+                _t_val = _sem * stdtrit( len(self.opts.in_files)-1 or 1, 1 - self.opts.alpha)
+                writer.writerow(['%0.4f'%x,
+                                 '%0.4f'%self.NDCHists[x]['hist']['mean'],
+                                 '%0.4f'%self.NDCHists[x]['hist']['std'],
+                                 "%0.4f"% _t_val])
 
     def WriteVT(self):
         '''Write the transition voltage data plotted against potential (not 1/V).'''
