0282be4ac36671525b4f76c70154d1d88d1ce0f5
Ryan Chiechi <rchiech@ncsu.edu>
2022-03-22 09:52:25 -0400

    Cleanup tooltips

--- gui/prefs.py
+++ gui/prefs.py
@@ -1,13 +1,12 @@
 '''A separate preferences window for settings Opts'''
 
 import tkinter.ttk as tk
-from tkinter import Toplevel,Y,BOTTOM,LEFT,RIGHT,END,BOTH
+from tkinter import Toplevel, Y, BOTTOM, LEFT, RIGHT, END, BOTH
 from gui.colors import GREY
 from gui.tooltip import createToolTip
 
-# pylint: disable=attribute-defined-outside-init,missing-function-docstring,line-too-long
+BOOLMAP = {1: True, 0: False}
 
-BOOLMAP = {1:True, 0:False}
 
 class PreferencesWindow(Toplevel):
     '''A new window to hold the preferences pane'''
@@ -37,65 +36,68 @@ class PreferencesFrame(tk.Frame):
         self.ButtonFrame = tk.Frame(self)
         self.LeftOptionsFrame = tk.Frame(self)
         self.RightOptionsFrame = tk.Frame(self)
-        self.ButtonFrame.pack(side=BOTTOM,fill=None)
-        self.LeftOptionsFrame.pack(side=LEFT,fill=Y)
-        self.RightOptionsFrame.pack(side=RIGHT,fill=Y)
+        self.ButtonFrame.pack(side=BOTTOM, fill=None)
+        self.LeftOptionsFrame.pack(side=LEFT, fill=Y)
+        self.RightOptionsFrame.pack(side=RIGHT, fill=Y)
 
     def __createButtons(self):
 
         buttons = [
-               {'name':'Save','text':'Save','side':BOTTOM},
+               {'name': 'Save', 'text': 'Save', 'side': BOTTOM},
                ]
 
         for _b in buttons:
             button = tk.Button(self.ButtonFrame)
-            button.config(text=_b['text'],command=getattr(self,_b['name']+'Click'))
+            button.config(text=_b['text'], command=getattr(self, _b['name']+'Click'))
             button.pack(side=_b['side'])
-            setattr(self,'Button'+_b['name'],button)
+            setattr(self, 'Button'+_b['name'], button)
 
     def __createOptions(self):
         lbls = [
             {'name': 'Columns', 'text': "Columns to parse:",
              'tooltip': 'Columns from input data to parse as X/Y data.',
-             'frame':self.LeftOptionsFrame},
+             'frame': self.LeftOptionsFrame},
             {'name': 'Vcutoff', 'text': "Cuttoff for d2J/dV2:",
              'tooltip': "Check the values of d2J/dV2 between |vcutoff| and Vmin/Vmax for line-shape filtering. Set to -1 for Vmin/Vmax.",
-             'frame':self.LeftOptionsFrame},
+             'frame': self.LeftOptionsFrame},
             {'name': 'Lagcutoff', 'text': "Cuttoff for lag plot filter:",
-             'tooltip': "Throw out J-values whose euclidian distance from a linear fit of the lag plot exceed this value for computing filtered histograms.",
-             'frame':self.LeftOptionsFrame},
+             'tooltip': """Throw out J-values whose euclidian distance from a linear fit of the lag plot
+            exceed this value for computing filtered histograms.""",
+             'frame': self.LeftOptionsFrame},
             {'name': 'Smooth', 'text': "Smoothing parameter:",
-             'tooltip': "The cutoff value for the residual squares (the difference between experimental data points and the fit). The default is 1e-12. Set to 0 to disable smoothing.",
-             'frame':self.LeftOptionsFrame},
+             'tooltip': """The cutoff value for the residual squares (the difference between experimental data points and the fit).
+             The default is 1e-12. Set to 0 to disable smoothing.""",
+             'frame': self.LeftOptionsFrame},
             {'name': 'Gminmax', 'text': "Y-scale for G heatmap:",
-                 'tooltip': "Set Ymin,Ymax for the heapmap plot (lower-left of plot output).",
-             'frame':self.LeftOptionsFrame},
+             'tooltip': "Set Ymin,Ymax for the heapmap plot (lower-left of plot output).",
+             'frame': self.LeftOptionsFrame},
             {'name': 'NDCminmax', 'text': "Y-scale for NDC heatmap:",
-                 'tooltip': "Set Ymin,Ymax for the heapmap plot (lower-left of plot output).",
-             'frame':self.LeftOptionsFrame},
+             'tooltip': "Set Ymin,Ymax for the heapmap plot (lower-left of plot output).",
+             'frame': self.LeftOptionsFrame},
             {'name': 'Bins', 'text': "Bins for J/R Histograms:",
              'tooltip': "Set binning for histograms of J and R.",
-             'frame':self.LeftOptionsFrame},
+             'frame': self.LeftOptionsFrame},
             {'name': 'Heatmapbins', 'text': "Bins for G Histograms:",
              'tooltip': "Set binning for heatmap histograms.",
-             'frame':self.LeftOptionsFrame},
+             'frame': self.LeftOptionsFrame},
             {'name': 'Alpha', 'text': "⍺-value for CI:",
-              'tooltip': "The p-cutoff is 1-⍺, e.g., ⍺ = 0.05 => 95% cutoff.",
-             'frame':self.LeftOptionsFrame},
+             'tooltip': "The p-cutoff is 1-⍺, e.g., ⍺ = 0.05 => 95% cutoff.",
+             'frame': self.LeftOptionsFrame},
             {'name': 'MaxR', 'text': "Maximum log|R|/R value:",
-              'tooltip': "Maximum allowable value of log|R| or (R if -R). Values above MaxR will excluded COMPLETELY from processing!",
-             'frame':self.RightOptionsFrame},
-             {'name': 'Segments', 'text': "Number of segments:",
-               'tooltip': "Total number of segments in each J/V trace (4 for EGaIn usually)",
-              'frame':self.RightOptionsFrame},
+             'tooltip': """Maximum allowable value of log|R| or (R if -R).
+             Values above MaxR will excluded COMPLETELY from processing!""",
+             'frame': self.RightOptionsFrame},
+            {'name': 'Segments', 'text': "Number of segments:",
+             'tooltip': "Total number of segments in each J/V trace (4 for EGaIn usually)",
+             'frame': self.RightOptionsFrame},
             {'name': 'Degrees of Freedom', 'text': "Number of degrees of freedom:",
-               'tooltip': "Manually provide degrees of freedom instead of guessing from number of input files.",
-              'frame':self.RightOptionsFrame},
+             'tooltip': "Manually provide degrees of freedom instead of guessing from number of input files.",
+             'frame': self.RightOptionsFrame},
         ]
 
         i = 0
         for _l in lbls:
-            tk.Label(_l['frame'], text=_l['text']).grid(column=0,row=i)
+            tk.Label(_l['frame'], text=_l['text']).grid(column=0, row=i)
             entry = tk.Entry(_l['frame'],
                              width=8,
                              validate='focus',
@@ -104,9 +106,9 @@ class PreferencesFrame(tk.Frame):
             entry.bind("<Leave>", self.checkOptions)
             entry.bind("<Enter>", self.checkOptions)
             entry.bind("<Tab>", self.checkOptions)
-            entry.grid(column=0,row=i+1)
+            entry.grid(column=0, row=i+1)
             createToolTip(entry, _l['tooltip'])
-            setattr(self, 'Entry'+_l['name'].replace(' ',''), entry)
+            setattr(self, 'Entry'+_l['name'].replace(' ', ''), entry)
             i += 2
 
         self.checkOptions()
@@ -115,10 +117,10 @@ class PreferencesFrame(tk.Frame):
         self.checkOptions()
         self.master.destroy()
 
-    def checkOptions(self, event=None):  # pylint: disable=W0613
-        for n in (('Smooth',1e-12),('Bins',50),('Heatmapbins',25)):
+    def checkOptions(self, event=None):
+        for n in (('Smooth', 1e-12), ('Bins', 50), ('Heatmapbins', 25)):
             try:
-                var = getattr(self,'Entry'+n[0]).get()
+                var = getattr(self, 'Entry'+n[0]).get()
                 if 'Smooth' in n:
                     var = float(var)
                 else:
@@ -129,11 +131,11 @@ class PreferencesFrame(tk.Frame):
                 var = int(0)
             if 'Smooth' in n:
                 if var >= 0:
-                    setattr(self.opts,n[0].lower(),var)
+                    setattr(self.opts, n[0].lower(), var)
             elif var > 0:
-                setattr(self.opts,n[0].lower(),var)
-            getattr(self,'Entry'+n[0]).delete(0,END)
-            getattr(self,'Entry'+n[0]).insert(0,getattr(self.opts,n[0].lower()))
+                setattr(self.opts, n[0].lower(), var)
+            getattr(self, 'Entry'+n[0]).delete(0, END)
+            getattr(self, 'Entry'+n[0]).insert(0, getattr(self.opts, n[0].lower()))
 
         self.checkGminmaxEntry()
         self.checkNDCminmaxEntry()
@@ -156,9 +158,9 @@ class PreferencesFrame(tk.Frame):
 
         self.EntryVcutoff.delete(0, END)  # pylint: disable=E1101
         if self.opts.vcutoff > 0:
-            self.EntryVcutoff.insert(0,self.opts.vcutoff)  # pylint: disable=E1101
+            self.EntryVcutoff.insert(0, self.opts.vcutoff)  # pylint: disable=E1101
         else:
-            self.EntryVcutoff.insert(0,"Vmax")  # pylint: disable=E1101
+            self.EntryVcutoff.insert(0, "Vmax")  # pylint: disable=E1101
 
     def checkLagcutoffEntry(self):
         try:
@@ -168,7 +170,7 @@ class PreferencesFrame(tk.Frame):
             self.opts.lagcutoff = 0.1
 
         self.EntryLagcutoff.delete(0, END)  # pylint: disable=E1101
-        self.EntryLagcutoff.insert(0,self.opts.lagcutoff)  # pylint: disable=E1101
+        self.EntryLagcutoff.insert(0, self.opts.lagcutoff)  # pylint: disable=E1101
 
     def checkColumnEntry(self):
         try:
@@ -224,7 +226,7 @@ class PreferencesFrame(tk.Frame):
             pass
         self.EntrySegments.delete(0, END)
         self.EntrySegments.insert(0, self.opts.segments)
-        
+
     def checkDegreesofFreedomEntry(self):
         try:
             _degfree = int(self.EntryDegreesofFreedom.get())
@@ -234,4 +236,3 @@ class PreferencesFrame(tk.Frame):
             pass
         self.EntryDegreesofFreedom.delete(0, END)
         self.EntryDegreesofFreedom.insert(0, self.opts.degfree)
-        
