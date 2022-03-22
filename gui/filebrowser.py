9dc6fe35b021e3efb728b1ce614edcd8303e177d
Ryan Chiechi <rchiech@ncsu.edu>
2022-03-22 09:41:55 -0400

    Fix J/R selection bug in GUI

--- gui/filebrowser.py
+++ gui/filebrowser.py
@@ -26,27 +26,26 @@ import logging
 
 import tkinter.ttk as tk
 from tkinter import filedialog
-from tkinter import Text,IntVar,StringVar,Listbox
-from tkinter import N,S,E,W,X,Y # pylint: disable=unused-import
-from tkinter import TOP,BOTTOM,LEFT,RIGHT # pylint: disable=unused-import
-from tkinter import END,BOTH,VERTICAL,HORIZONTAL # pylint: disable=unused-import
-from tkinter import EXTENDED,RAISED,DISABLED,NORMAL # pylint: disable=unused-import
+from tkinter import Text, IntVar, StringVar, Listbox
+from tkinter import N, S, E, W, X, Y  # pylint: disable=unused-import
+from tkinter import TOP, BOTTOM, LEFT, RIGHT  # pylint: disable=unused-import
+from tkinter import END, BOTH, VERTICAL, HORIZONTAL  # pylint: disable=unused-import
+from tkinter import EXTENDED, RAISED, DISABLED, NORMAL  # pylint: disable=unused-import
 from tkinter.font import Font
 
 from gui.prefs import PreferencesWindow
-from gui.colors import BLACK,YELLOW,WHITE,RED,TEAL,GREEN,BLUE,GREY # pylint: disable=unused-import
+from gui.colors import BLACK, YELLOW, WHITE, RED, TEAL, GREEN, BLUE, GREY  # pylint: disable=unused-import
 from gui.tooltip import createToolTip
 from gaussfit import Parse
-from gaussfit.output import Writer,Plotter
+from gaussfit.output import Writer, Plotter
 from gaussfit.logger import GUIHandler
 
-# pylint: disable=attribute-defined-outside-init,missing-function-docstring
 
 class ParseThread(threading.Thread):
     '''A Thread object to run the parse in so it
        doesn't block the main GUI thread.'''
 
-    def __init__(self,opts,parser):
+    def __init__(self, opts, parser):
         threading.Thread.__init__(self)
         self.parser = parser
         self.opts = opts
@@ -61,7 +60,7 @@ class ChooseFiles(tk.Frame):
 
     def __init__(self, opts, master=None):
         tk.Frame.__init__(self, master)
-        self.boolmap = {1:True, 0:False}
+        self.boolmap = {1: True, 0: False}
         try:
             self.last_input_path = os.getcwd()
         except KeyError:
@@ -91,113 +90,113 @@ class ChooseFiles(tk.Frame):
         self.Logging = Text(self.LoggingFrame, height=20, width=0,
                             bg=BLACK, fg=WHITE, yscrollcommand=yScroll.set)
         yScroll['command'] = self.Logging.yview
-        self.Logging.pack(side=LEFT,fill=BOTH,expand=True)
-        yScroll.pack(side=RIGHT,fill=Y)
+        self.Logging.pack(side=LEFT, fill=BOTH, expand=True)
+        yScroll.pack(side=RIGHT, fill=Y)
 
         self.__createButtons()
         self.__createFileListBox()
         self.__createOptions()
 
-        self.ButtonFrame.pack(side=BOTTOM,fill=None)
+        self.ButtonFrame.pack(side=BOTTOM, fill=None)
         self.FileListBoxFrame.pack(side=TOP, fil=BOTH, expand=True)
         # self.LeftOptionsFrame.pack(side=LEFT,fill=Y)
-        self.RightOptionsFrame.pack(side=RIGHT,fill=Y)
+        self.RightOptionsFrame.pack(side=RIGHT, fill=Y)
         self.LoggingFrame.pack(side=BOTTOM, fill=BOTH)
         self.logger = logging.getLogger('parser.gui')
         self.handler = GUIHandler(self.Logging)
         self.logger.addHandler(self.handler)
-        self.logger.setLevel(getattr(logging,self.opts.loglevel.upper()))
+        self.logger.setLevel(getattr(logging, self.opts.loglevel.upper()))
         self.logger.info("Config file:%s", self.opts.configfile)
         self.handler.flush()
         self.updateFileListBox()
 
     def __createButtons(self):
 
-        buttons = [{'name':'Quit','text':'QUIT','command':'Quit','side':BOTTOM},
-                   {'name':'SpawnInputDialog','text':'Add Input Files','side':LEFT},
-                   {'name':'RemoveFile','text':'Remove Files','side':LEFT},
-                   {'name':'SpawnOutputDialog','text':'Choose Output Directory','side':LEFT},
-                   {'name':'Settings','text':'Settings','side':LEFT},
-                   {'name':'Parse','text':'Parse!','side':LEFT}
+        buttons = [{'name': 'Quit', 'text': 'QUIT', 'command': 'Quit', 'side': BOTTOM},
+                   {'name': 'SpawnInputDialog', 'text': 'Add Input Files', 'side': LEFT},
+                   {'name': 'RemoveFile', 'text': 'Remove Files', 'side': LEFT},
+                   {'name': 'SpawnOutputDialog', 'text': 'Choose Output Directory', 'side': LEFT},
+                   {'name': 'Settings', 'text': 'Settings', 'side': LEFT},
+                   {'name': 'Parse', 'text': 'Parse!', 'side': LEFT}
                    ]
 
         for b in buttons:
             button = tk.Button(self.ButtonFrame)
-            button.config(text=b['text'],command=getattr(self,b['name']+'Click'))
+            button.config(text=b['text'], command=getattr(self, b['name']+'Click'))
             button.pack(side=b['side'])
-            setattr(self,'Button'+b['name'],button)
+            setattr(self, 'Button'+b['name'], button)
 
     def __createOptions(self):
         '''Create the widgets for options and use setattr to assign them to self.'''
-        self.checks = [{'name':'plot','text':'Plot','row':1,
-                        'tooltip':"Show summary plots after parsing."},
-                       {'name':'write','text':'Write','row':2,
-                        'tooltip':"Write results to text files after parsing."},
-                       {'name':'skipohmic','text':'Skip bad dJ/dV','row':3,
-                        'tooltip':'Skip plots with d2J/dV2 < 0 between Vcutoff and Vmin/Vmax.'},
-                       {'name':'interpolateminfn','text':'Interpolate FN','row':4,
-                        'tooltip':'Find FN from the minimum of the derivative.'},
-                       {'name':'logr','text':'Use log|R|','row':5,
-                        'tooltip':'Use log |R| instead of |R| when computing histograms.'},
-                       {'name':'lorenzian','text':'Lorenzian','row':6,
-                        'tooltip':'Fit a Lorenzian instead of a Gaussian.'},
-                       {'name':'tracebyfile','text':'AFM Data','row':7,
-                        'tooltip':'Each file contains one (foward/backward) trace.'},
-                       {'name':'nolag','text':'No lag plots.','row':8,
-                        'tooltip':'Some data (often AFM) produces bad lag plots very slowly.'}
+        self.checks = [{'name': 'plot', 'text': 'Plot', 'row': 1,
+                        'tooltip': "Show summary plots after parsing."},
+                       {'name': 'write', 'text': 'Write', 'row': 2,
+                        'tooltip': "Write results to text files after parsing."},
+                       {'name': 'skipohmic', 'text': 'Skip bad dJ/dV', 'row': 3,
+                        'tooltip': 'Skip plots with d2J/dV2 < 0 between Vcutoff and Vmin/Vmax.'},
+                       {'name': 'interpolateminfn', 'text': 'Interpolate FN', 'row': 4,
+                        'tooltip': 'Find FN from the minimum of the derivative.'},
+                       {'name': 'logr', 'text': 'Use log|R|', 'row': 5,
+                        'tooltip': 'Use log |R| instead of |R| when computing histograms.'},
+                       {'name': 'lorenzian', 'text': 'Lorenzian', 'row': 6,
+                        'tooltip': 'Fit a Lorenzian instead of a Gaussian.'},
+                       {'name': 'tracebyfile', 'text': 'AFM Data', 'row': 7,
+                        'tooltip': 'Each file contains one (foward/backward) trace.'},
+                       {'name': 'nolag', 'text': 'No lag plots.', 'row': 8,
+                        'tooltip': 'Some data (often AFM) produces bad lag plots very slowly.'}
                        ]
 
         for _c in self.checks:
-            setattr(self,_c['name'],IntVar())
+            setattr(self, _c['name'], IntVar())
             check = tk.Checkbutton(self.RightOptionsFrame, text=_c['text'],
-                                   variable=getattr(self,_c['name']), command=self.checkOptions)
-            check.grid(column=0,row=_c['row'],sticky=W)
-            createToolTip(check,_c['tooltip'])
-            setattr(self,'Check_'+_c['name'],check)
-            if getattr(self.opts,_c['name']):
-                getattr(self,_c['name']).set(1)
+                                   variable=getattr(self, _c['name']), command=self.checkOptions)
+            check.grid(column=0, row=_c['row'], sticky=W)
+            createToolTip(check, _c['tooltip'])
+            setattr(self, 'Check_'+_c['name'], check)
+            if getattr(self.opts, _c['name']):
+                getattr(self, _c['name']).set(1)
 
         rowidx = len(self.checks)+1
 
         tk.Label(self.RightOptionsFrame, text="Output file base name:").grid(
-            column=0,row=rowidx)
+            column=0, row=rowidx)
         self.OutputFileName = tk.Entry(self.RightOptionsFrame, width=20,
-                                       font=Font(size=8,slant='italic'))
-        for n in ('<Return>','<Leave>','<Enter>'):
+                                       font=Font(size=8, slant='italic'))
+        for n in ('<Return>', '<Leave>', '<Enter>'):
             self.OutputFileName.bind(n, self.checkOutputFileName)
-        self.OutputFileName.grid(column=0,row=rowidx+1)
+        self.OutputFileName.grid(column=0, row=rowidx+1)
 
         if self.opts.outfile:
-            self.OutputFileName.insert(0,self.opts.outfile)
+            self.OutputFileName.insert(0, self.opts.outfile)
 
         tk.Label(self.RightOptionsFrame, text="Data to plot:").grid(
-            column=0,row=rowidx+2,sticky=W)
+            column=0, row=rowidx+2, sticky=W)
         self.OptionsPlotsString = StringVar()
         self.OptionsPlotsString.set(self.opts.plots)
         self.OptionPlots = tk.OptionMenu(self.RightOptionsFrame,
-                                         self.OptionsPlotsString,'J','R',
+                                         self.OptionsPlotsString, self.opts.plots, 'J', 'R',
                                          command=self.checkOptions)
-        self.OptionPlots.grid(column=0,row=rowidx+3,sticky=W)
+        self.OptionPlots.grid(column=0, row=rowidx+3, sticky=W)
 
         tk.Label(self.RightOptionsFrame, text="Histogram to plot:").grid(
-            column=0,row=rowidx+4,sticky=W)
+            column=0, row=rowidx+4, sticky=W)
         self.OptionsHistPlotsString = StringVar()
         self.OptionsHistPlotsString.set(self.opts.histplots)
         self.OptionHistPlots = tk.OptionMenu(self.RightOptionsFrame,
-                                             self.OptionsHistPlotsString, self.opts.histplots, 'NDC','G',
+                                             self.OptionsHistPlotsString, self.opts.histplots, 'NDC', 'G',
                                              command=self.checkOptions)
         createToolTip(self.OptionHistPlots, '(Normalized) Differential Conductance')
-        self.OptionHistPlots.grid(column=0,row=rowidx+5,sticky=W)
+        self.OptionHistPlots.grid(column=0, row=rowidx+5, sticky=W)
 
         tk.Label(self.RightOptionsFrame, text="Derivative for heatmap:").grid(
-            column=0,row=rowidx+6,sticky=W)
+            column=0, row=rowidx+6, sticky=W)
         self.OptionsHeatmapdString = StringVar()
         self.OptionsHeatmapdString.set(self.opts.heatmapd)
         self.OptionHeatmapd = tk.OptionMenu(self.RightOptionsFrame,
-                                            self.OptionsHeatmapdString, self.opts.heatmapd,'0','1','2',
+                                            self.OptionsHeatmapdString, self.opts.heatmapd, '0', '1', '2',
                                             command=self.checkOptions)
         createToolTip(self.OptionHeatmapd, '0 is equivalent to LogJ, 1 is Log|dJ/dV|, etc.')
-        self.OptionHeatmapd.grid(column=0,row=rowidx+7,sticky=W)
+        self.OptionHeatmapd.grid(column=0, row=rowidx+7, sticky=W)
 
         self.checkOptions()
 
@@ -205,13 +204,13 @@ class ChooseFiles(tk.Frame):
         self.FileListBoxFrameLabelVar = StringVar()
         self.FileListBoxFrameLabel = tk.Label(self.FileListBoxFrame,
                                               textvariable=self.FileListBoxFrameLabelVar,
-                                              font=Font(size=10,weight="bold"))
-        self.FileListBoxFrameLabel.pack(side=TOP,fill=X)
+                                              font=Font(size=10, weight="bold"))
+        self.FileListBoxFrameLabel.pack(side=TOP, fill=X)
 
         yScroll = tk.Scrollbar(self.FileListBoxFrame, orient=VERTICAL)
-        yScroll.pack(side=RIGHT,fill=Y)
+        yScroll.pack(side=RIGHT, fill=Y)
         xScroll = tk.Scrollbar(self.FileListBoxFrame, orient=HORIZONTAL)
-        xScroll.pack(side=BOTTOM,fill=X)
+        xScroll.pack(side=BOTTOM, fill=X)
         self.filelist = StringVar()
         self.FileListBox = Listbox(self.FileListBoxFrame,
                                    listvariable=self.filelist, selectmode=EXTENDED,
@@ -228,7 +227,7 @@ class ChooseFiles(tk.Frame):
     def ToFront(self):
         '''Try to bring the main window to the front on different platforms'''
         if platform.system() == "Darwin":
-            os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')  # pylint: disable=line-too-long
+            os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
         else:
             self.master.attributes('-topmost', 1)
             self.master.attributes('-topmost', 0)
@@ -255,7 +254,7 @@ class ChooseFiles(tk.Frame):
             self.ButtonParse['state'] = DISABLED  # pylint: disable=E1101
             for _t in self.gothreads:
                 if _t.is_alive():
-                    self.ButtonParse.after('500',self.Parse)  # pylint: disable=E1101
+                    self.ButtonParse.after('500', self.Parse)  # pylint: disable=E1101
                     return
             self.logger.info("Parse complete!")
             gothread = self.gothreads.pop()
@@ -274,10 +273,10 @@ class ChooseFiles(tk.Frame):
                     self.logger.error("Cannot import matplotlib! %s", str(msg), exc_info=False)
         else:
             if self.opts.in_files:
-                parser = Parse(self.opts,handler=self.handler)
-                self.gothreads.append(ParseThread(self.opts,parser))
+                parser = Parse(self.opts, handler=self.handler)
+                self.gothreads.append(ParseThread(self.opts, parser))
                 self.gothreads[-1].start()
-                self.ButtonParse.after('500',self.Parse)  # pylint: disable=E1101
+                self.ButtonParse.after('500', self.Parse)  # pylint: disable=E1101
             else:
                 self.logger.warning("No input files!")
         self.postParse()
@@ -290,7 +289,7 @@ class ChooseFiles(tk.Frame):
         filelist = []
         for i in range(0, len(self.opts.in_files)):
             for s in selected:
-                if self.opts.in_files[i].replace(" ","_") == s:
+                if self.opts.in_files[i].replace(" ", "_") == s:
                     todel.append(i)
 
         for i in range(0, len(self.opts.in_files)):
@@ -298,7 +297,7 @@ class ChooseFiles(tk.Frame):
                 filelist.append(self.opts.in_files[i])
         self.opts.in_files = filelist
         self.updateFileListBox()
-        self.FileListBox.selection_clear(0,END)
+        self.FileListBox.selection_clear(0, END)
 
     def SpawnInputDialogClick(self):
         self.checkOptions()
@@ -306,8 +305,8 @@ class ChooseFiles(tk.Frame):
             title="Files to parse",
             multiple=True,
             initialdir=self.last_input_path,
-            filetypes=[('Data files','*_data.txt'),
-                       ('Text files','*.txt'),('All files', '*')])
+            filetypes=[('Data files', '*_data.txt'),
+                       ('Text files', '*.txt'), ('All files', '*')])
         if len(self.opts.in_files):
             self.last_input_path = os.path.split(self.opts.in_files[0])[0]
             if not self.outdir:
@@ -316,11 +315,11 @@ class ChooseFiles(tk.Frame):
             if not self.opts.outfile:
                 self.OutputFileName.delete(0, END)
                 self.OutputFileName.insert(0, os.path.basename(
-                    self.opts.in_files[-1]).lower().replace('.txt',''))
+                    self.opts.in_files[-1]).lower().replace('.txt', ''))
                 self.checkOutputFileName()
 
     def updateFileListBox(self):
-        self.filelist.set(" ".join([x.replace(" ","_") for x in self.opts.in_files]))
+        self.filelist.set(" ".join([x.replace(" ", "_") for x in self.opts.in_files]))
 
     def SpawnOutputDialogClick(self):
         outdir = filedialog.askdirectory(title="Select Output File(s)",
@@ -335,7 +334,7 @@ class ChooseFiles(tk.Frame):
 
     def checkOptions(self, event=None):  # pylint: disable=W0613
         for c in self.checks:
-            setattr(self.opts,c['name'],self.boolmap[getattr(self,c['name']).get()])
+            setattr(self.opts, c['name'], self.boolmap[getattr(self, c['name']).get()])
 
         if self.OptionsHistPlotsString.get() == 'NDC':
             self.OptionHeatmapd["state"] = DISABLED
